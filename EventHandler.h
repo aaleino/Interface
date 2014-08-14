43115845e97f4bbcbe787184a2cf9e37ab398c9b
aaleino <rekola@Mikaels-Mac-mini.local>
2014-08-14 22:57:35 +0300

    Testing github for the first time

---
+++ EventHandler.h
@@ -0,0 +1,13 @@
+//
+//  EventHandler.h
+//  IpadInterface
+//
+//  Created by Aleksi Leino on 14/08/14.
+//
+
+#ifndef _EventHandler_h
+#define _EventHandler_h
+
+class EventHandler;
+
+#endif
---
+++ OS.h
@@ -0,0 +1,13 @@
+//
+//  OS.h
+//  IpadInterface
+//
+//  Created by Aleksi Leino on 14/08/14.
+//
+
+#ifndef IpadInterface_OS_h
+#define IpadInterface_OS_h
+
+class OS;
+
+#endif
