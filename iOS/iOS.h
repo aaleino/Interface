1253fdeeb86c51ae1c5f5f93e9f262fb3113aeaf
aaleino <aleksi.leino@gmail.com>
2014-08-15 11:21:53 +0300

    Update iOS.h

--- iOS/iOS.h
+++ iOS/iOS.h
@@ -3,4 +3,12 @@
 
 #include "../Generic/OS.h"
 
-class iOS : public OS
+class iOS : public OS {
+  public:
+    class iOS() {}
+  
+    void pressBegin(int x, int y);
+    void pressMoved(int x, int y);
+    void pressEnd(int x, int y);
+};
+
