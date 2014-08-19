ed0ae02124dbb45a2fe815d61b3083e3d1e3a753
Mikael Rekola <mikael.rekola@gmail.com>
2014-08-19 03:18:26 +0300

    add dispatch method, add flags is_handled and redraw_requestes and their methods

--- Generic/Event.h
+++ Generic/Event.h
@@ -1,11 +1,24 @@
-
 #ifndef _EVENT_H_
 #define _EVENT_H_
 
-class Event {
-    public:
-		Event() {};
+#include "EventHandler.h"
 
+class Event {
+ public:
+  Event() {};
+  virtual ~Event() { }
+  
+  virtual void dispatch(EventHandler & evh) = 0;
+
+  void setIsHandled() { is_handled = true; }
+  void requestRedraw() { redraw_requested = true; }  
+  
+  bool isHandled() const { return is_handled; }
+  bool isRedrawRequested() const { return redraw_requested; }
+
+ private:
+  bool is_handled = false;
+  bool redraw_requested = false;
 };
 
 // For mouse and touch
@@ -21,8 +34,12 @@ class PressEvent : public Event {
 		void sety(int y) { loc_y = y; }
         void setIdentifier(int _identifier) { identifier = _identifier;}
 
+	void dispatch(EventHandler & evh) {
+	  evh.pressMove(*this);
+	}
+
    private:
-		int loc_x, loc_y;
+	int loc_x, loc_y;
         int identifier; // for multiple simultaneous presses
 };
 
